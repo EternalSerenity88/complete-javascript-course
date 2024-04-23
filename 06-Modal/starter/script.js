353e871186c8983ee41c73b0c204a73df5ec7d3a
Skylar Carrington <149277135+EternalSerenity88@users.noreply.github.com>
2024-04-23 18:56:28 +0100

    Project_2_ModalWindow is complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,12 +1,35 @@
 'use strict';
 
+// Assigning DOM elements to variables
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloserModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+// functions to close/open the window and blured overlay, by manipulating the visibilty of the classes
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Using loop in order to apply 'EventListener' to every button
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-  });
+  btnsOpenModal[i].addEventListener('click', showModal);
+
+btnCloserModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// Using specific key to do action (Escape, to close opened window)
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
