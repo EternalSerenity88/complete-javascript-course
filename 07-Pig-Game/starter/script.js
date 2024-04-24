93521b37bb9dce4dd3b8f9d28d90c4fc2af522de
Skylar Carrington <149277135+EternalSerenity88@users.noreply.github.com>
2024-04-24 10:25:23 +0100

    Figuring out new game function

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -23,32 +23,87 @@ diceEl.classList.add('hidden');
 const scores = [0, 0]; // To hold total scores for each player, [0] - first player's score, [1] - second player's score
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true; // To make the game stop after winning
+
+const switchPlayer = function () {
+  // Reset score of current player to zero
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  // Switch to next player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // For manipulation with CSS for a current player
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Genereting a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1. Genereting a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  // Connecting randomly generated number with coressponding png!
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    // Connecting randomly generated number with coressponding png!
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    // Display updated score for current player
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Reset score of current player to zero
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    // Switch to next player
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // For manipulation with CSS for a current player
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      // Display updated score for current player
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // score[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  // Reset total scores to zero
+  scores[0] = 0;
+  scores[1] = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  // Reset current scores to zero
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  playing = true;
+
+  // Reset overlay
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
 });
