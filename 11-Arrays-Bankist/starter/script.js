0b47f1599f0574d5a9d90e87bf1baccf6833536b
Skylar Carrington <149277135+EternalSerenity88@users.noreply.github.com>
2024-05-07 13:06:38 +0100

    project banking started

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,34 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Passing an arrays of transaction data (movements)
+const displayMovements = function (movements) {
+  // Sets data in main index file to zero
+  containerMovements.innerHTML = '';
+
+  // index for a sequence of transaction starting from the bottom
+  movements.forEach(function (mov, i) {
+    // for transaction CSS manipulation
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>;
+    `;
+
+    // to insert html element to the DOM, sending it to a parent class variable (containerMovements)
+    // first argument, is the position relatively to parent element (4 options-see manual)
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+// To display transactions for account1
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +102,91 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2)); // c, d, e
+console.log(arr.slice(2, 4)); // c, d
+console.log(arr.slice(-2)); // d, e
+console.log(arr.slice(-1)); // e
+console.log(arr.slice(1, -2)); // b, c
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE (MUTATES ORIGINAL ARRAY INSTEAD OF CREATING NEW ONE)
+// numbers in brackets are elements to delete
+// console.log(arr.splice(2))
+console.log(arr);
+arr.splice(-1); // deletes 0 and 1
+console.log(arr);
+arr.splice(1, 2); //second number is the number of elements to remove, including first argument!!!
+console.log(arr);
+
+// REVERSE - mutates original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letter = arr.concat(arr2);
+console.log(letter);
+console.log([...arr, ...arr2]); // same as concat
+
+//JOIN
+console.log(letter.join('-'));
+
+// NEW AT METHOD
+arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0)); // same thing
+
+// traditional last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1)); // new way
+
+console.log('jonas'.at(0)); // also works on strings
+console.log('jonas'.at(-1));
+
+// Looping: forEach - Arrays
+
+const movements1 = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements1.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`); // Math.abs for asbsolute value, removing the sign
+  }
+}
+
+console.log('------- FOREACH -------');
+movements1.forEach(function (current_element, i, array) {
+  if (current_element > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${current_element}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(current_element)}`); // Math.abs for asbsolute value, removing the sign
+  }
+});
+
+// Looping: forEach - Maps
+const currencies1 = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Looping: forEach - Sets
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  // Sets have no keys or indexes,
+  // therefore, second argument is irrelevant but must be included for consistency sake
+  console.log(`${value}: ${value}`);
+});
