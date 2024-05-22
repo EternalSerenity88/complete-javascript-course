b380d7b583d2656a9bde4ebb0dda21e61b3f83d3
Skylar Carrington <149277135+EternalSerenity88@users.noreply.github.com>
2024-05-22 09:34:09 +0100

    carry on

--- 11-Arrays-Bankist/starter/challenges.js
+++ 11-Arrays-Bankist/starter/challenges.js
@@ -49,3 +49,15 @@ const calcAverageHumanAge = function (dogAge) {
 };
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+// 3. CHALLENGE ==========================================================================
+
+const calcAverageHumanAge2 = dogAge2 =>
+  dogAge2
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
